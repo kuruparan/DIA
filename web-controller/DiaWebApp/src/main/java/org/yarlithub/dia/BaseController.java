538a01f0c41b2ca45e73e77f89c1f89e06c6ee70
Jayakrishnan <jayakrishnanr@hsenidmobile.com>
2014-05-23 11:46:23 +0530

    hellow world wep-app using spring and java servlet

---
+++ web-controller/DiaWebApp/src/main/java/org/yarlithub/dia/BaseController.java
@@ -0,0 +1,35 @@
+package org.yarlithub.dia;
+
+/**
+ * Created by jaykrish on 5/23/14.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+//Maps the request after host:port/dia/
+public class BaseController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+
+        model.addAttribute("message", "DIA Web Project + Spring 3 MVC - welcome()");
+
+        //Spring uses InternalResourceViewResolver and return back index.jsp
+        return "index";
+
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String name, ModelMap model) {
+
+        model.addAttribute("message", "DIA Web Project + Spring 3 MVC - you are now at page: " + name);
+        return "index";
+
+    }
+}
